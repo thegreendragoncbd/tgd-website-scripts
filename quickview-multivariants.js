c49677c86b68cf4b4e2a539243a39495c148469a
Jesus Perez <jesusmpds18@gmail.com>
2023-07-17 12:45:23 -0300

    fix: isWholesale check missing in updateProductInfo function

--- quickview-multivariants.js
+++ quickview-multivariants.js
@@ -794,7 +794,7 @@ if (isProductCMSPage(URL_PATH) || isProductListPage()) {
           switch (key) {
             case "inventory":
               // Update max quantity
-              if (!isWholesale && !isWholesalePage) {
+              if (!isWholesale || !isWholesalePage) {
                 element.querySelector(`input[name="quantity_max"]`).value =
                   selectedProductVariantInfo["inventory"];
               }
