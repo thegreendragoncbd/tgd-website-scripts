fd7c4970a39ccd8073b285fdc867a9e9b9fb2ae0
thegreendragoncbd <110121705+thegreendragoncbd@users.noreply.github.com>
2024-10-25 17:46:04 -0400

    Update quickview-multivariants.js

--- quickview-multivariants.js
+++ quickview-multivariants.js
@@ -921,9 +921,8 @@ if (isProductCMSPage(URL_PATH) || isProductListPage()) {
           var lcVariantGroupName=variant.toLowerCase();
           selectDropdown.id=`variant-${lcVariantGroupName}-select-dropdown`;
           selectDropdown.innerHTML="Please Select<div id='select-arrow' style='-ms-transform: rotate(270deg);-webkit-transform: rotate(270deg); transform: rotate(270deg);display: inline-block;padding-right: 10px;float:right;font-style: normal;'>&#x276E;</div>";
-          
-          const ddContent=$(`#variants-${lcVariantGroupName}`);
-          const ddParent=$('#'+lcVariantGroupName+"-wrapper"); 
+          const ddContent=document.getElementById("variants-"+lcVariantGroupName);
+          const ddParent=document.getElementById(lcVariantGroupName+"-wrapper");
           ddParent.insertBefore(selectDropdown, ddContent);
           ddParent.style.position='relative';
           ddContent.style.position='absolute';
