ef34c7d54cd24b8fd86b35c03e22f19d90a3dacc
thegreendragoncbd <110121705+thegreendragoncbd@users.noreply.github.com>
2024-10-30 17:02:28 -0400

    Update quickview-multivariants.js

--- quickview-multivariants.js
+++ quickview-multivariants.js
@@ -954,7 +954,7 @@ if (isProductCMSPage(URL_PATH) || isProductListPage()) {
               $(`#variants-${lcVariantGroupName}`).hide();
               $(this).css("background-color","1#c1e9d4");
               selectDropdown.innerHTML=$(`#variants-${lcVariantGroupName} .radio-button-field.w-radio.is-active-inputactive span.radio-btn.w-form-label`).text()+"<div id='select-arrow' style='-ms-transform: rotate(270deg);-webkit-transform: rotate(270deg); transform: rotate(270deg);display: inline-block;padding-right: 10px;float:right;font-style: normal;'>&#x276E;</div>";
-              $(".w-form-formradioinput.w-form-formradioinput--inputType-custom.radio-button.w-radio-input.w--redirected-checked").css("background-color","#c1e9d4");
+              $("label .w-form-formradioinput.w-form-formradioinput--inputType-custom.radio-button.w-radio-input.w--redirected-checked").css("background-color","#c1e9d4");
             }, 100);
             
           });
