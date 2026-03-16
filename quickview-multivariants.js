debb0aa2254989668337cd3de3b6637829b751b9
thegreendragoncbd <110121705+thegreendragoncbd@users.noreply.github.com>
2026-03-16 13:25:55 -0400

    Add Sales/Deals page

--- quickview-multivariants.js
+++ quickview-multivariants.js
@@ -26,7 +26,8 @@ function isProductListPage() {
     URL_PATH.includes("/disposable-thc-vape-pens-carts") ||
     URL_PATH.includes("/empire-brands") || 
     URL_PATH.includes("/bulk-thca-flower-pounds-online") ||
-    URL_PATH.includes("/pages/")
+    URL_PATH.includes("/pages/") || 
+    URL_PATH.includes("/sales-deals/")
   );
 }
 function isProductCMSPage(URL_PATH) {
